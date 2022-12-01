a562fc4a9f82cf69db5d3c58fe655730e5fdd7ff
iremtopuzcu <iremtopuzcu@hotmail.com>
2022-12-01 17:42:25 +0300

    The sum of the two numbers entered

--- src/Question1.java
+++ src/Question1.java
@@ -7,12 +7,12 @@ public class Question1 {
 		
 		int num1,num2,sum;
 		
-		System.out.println("please enter first number : ");
+		System.out.println("please enter first number  : ");
 		num1 = input.nextInt();
-		System.out.println("please enter second number : ");
+		System.out.println("please enter second number  : ");
 		num2 = input.nextInt();
 		sum = num1 + num2;
-		System.out.println("sum of two number is : "+sum);
+		System.out.println(" sum of two number is : "+sum);
 		
 		
 
