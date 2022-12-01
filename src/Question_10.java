1eb558c360ac94e2c1c43e0ebce5995823828551
iremtopuzcu <iremtopuzcu@hotmail.com>
2022-12-01 23:01:47 +0300

    Program to find whether the sum of the cubes of a 3 digit number is equal to the number itself

--- src/Question_10.java
+++ src/Question_10.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+import java.util.Math;
+// Program to find whether the sum of the cubes of a 3 digit number is equal to the number itself.
 
-public class Question_10 {
+public class Question10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int number,printt,x;
+		int total = 0;
+		System.out.println("please enter a number : ");
+		number = input.nextInt();
+		x = number;
+		while(number > 9) {
+			printt = number % 10;
+			total += Math.pow(printt, 3);
+			number /= 10;
+		}//end of while
+		total += Math.pow(number, 3);
+		if(total == x)
+			System.out.println("equal");
+		else
+			System.out.println("not equal");
 	}
 
 }
+
