717121fc6fe1cbbfc68dfd9a0895fddbfa4c038b
iremtopuzcu <iremtopuzcu@hotmail.com>
2022-12-02 23:06:35 +0300

    This is a Java Program to Reverse a Given Number

---
+++ src/Question_13.java
@@ -0,0 +1,28 @@
+//This is a Java Program to Reverse a Given Number.
+
+import java.util.Scanner;
+
+public class Question_13 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int number,x,sum=0;
+		System.out.println("please enter a number");
+		number = input.nextInt();
+		
+		while(number>0) {
+			x = number%10;
+			
+			sum = sum * 10 + x;
+			
+			number = number/10;
+			
+		}
+		
+		System.out.println("reverse : "+sum);
+		
+
+	}
+
+}
