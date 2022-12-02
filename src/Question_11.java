85b010beb4f3f88fa4d196bc46ea2b094812195d
iremtopuzcu <iremtopuzcu@hotmail.com>
2022-12-02 22:44:37 +0300

    A Java Program That Checks If A Given Integer Is Odd or Even

--- src/Question_11.java
+++ src/Question_11.java
@@ -1,8 +1,19 @@
+// A Java Program That Checks If A Given Integer Is Odd or Even
+import java.util.Scanner;
 
 public class Question_11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int number;
+		System.out.println("please enter a number : ");
+		number = input.nextInt();
+		
+		if(number%2 == 0) {
+			System.out.println("The number you entered is even.");
+		}
+		else
+			System.out.println("The number you entered is odd.");
 
 	}
 
