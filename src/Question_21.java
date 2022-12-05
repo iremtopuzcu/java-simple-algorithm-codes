86dd3e22ab1db9b72f33b6573fc9bbeb51db8bee
iremtopuzcu <iremtopuzcu@hotmail.com>
2022-12-06 00:53:09 +0300

    Enter the number for which you want to get the factorial

---
+++ src/Question_21.java
@@ -0,0 +1,32 @@
+//Java Program to Find the Factorial of a Number using Recursion
+
+import java.util.Scanner;
+
+public class Question_21 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int num,fac=1;
+		System.out.println("Enter the number for which you want to get the factorial  : ");
+		num = input.nextInt();
+		if (num>=0) 
+		{
+		  for(int i=1; i<=num ; i++)
+		  {
+			fac*= i;
+		  }
+		
+		}
+		
+		else
+		{
+			fac=0;
+			
+		}
+		
+		System.out.println(fac);
+	}
+
+}
